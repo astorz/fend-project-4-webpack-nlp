f86e95e80224f3fe005020b5b9b83296fc8e0e8d
‘Andreas <andreasstorz@hotmail.com>
2021-03-06 21:16:39 -0500

    Optimizing CSS in prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,10 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin'); 
+const TerserPlugin = require('terser-webpack-plugin'); 
+
 
 module.exports = {
     mode: 'production',
@@ -8,7 +12,10 @@ module.exports = {
     output: {
         libraryTarget: 'var',
         library: 'Client'
-    },  
+    },
+    optimization: {   
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+    }, 
     module: {
         rules: [
             {
@@ -18,14 +25,15 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-                }               
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            }
         ]
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" })
     ]
 }
